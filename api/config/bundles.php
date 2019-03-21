b8f8fc26104370aae5890b33753573e8b4c66e0c
Dimitri LAVAURY-COLLOT <dlavaury@e3r2p23.42.fr>
2019-03-21 21:09:01 +0100

    set db connection

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,4 +13,6 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
