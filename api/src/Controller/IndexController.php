87476cf1d575b768a1de4b0b3991755317bc34d8
lbarthon <lsbarthonet2@gmail.com>
2019-03-24 12:25:05 +0100

    Toujours le pire dev ._.

--- api/src/Controller/IndexController.php
+++ api/src/Controller/IndexController.php
@@ -61,7 +61,7 @@ class IndexController extends AbstractController
      * @Route("/torrent", name="test2")
      */
     public function dlTorrent() {
-        $transmission = new Transmission($this->config);
+        $transmission = new Transmission($this->transmissionConfig);
         $transmission->add('magnet:?xt=urn:btih:11A2AC68A11634E980F265CB1433C599D017A759&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337');
         return new JsonResponse($transmission->getStats());
     }
@@ -70,7 +70,7 @@ class IndexController extends AbstractController
      * @Route("/status", name="test3")
      */
     public function status() {
-        $transmission = new Transmission($this->config);
+        $transmission = new Transmission($this->transmissionConfig);
         return new JsonResponse($transmission->getStats());
     }
 }
