480d24ebee49f2e5ef91a03ad5e2d2cea33c29c9
lbarthon <lsbarthonet2@gmail.com>
2019-03-24 12:18:18 +0100

    ÇA MARCHE PRESQUE LETS GO

--- api/src/Controller/IndexController.php
+++ api/src/Controller/IndexController.php
@@ -57,18 +57,26 @@ class IndexController extends AbstractController
     public function dlTorrent() {
         $config = array(
             'host'     => 'http://127.0.0.1',
-            'endpoint' => '\/transmission\/',
+            'endpoint' => '/transmission/rpc',
             'username' => 'transmission',
             'password' => '12345678'
         );
         $this->transmission = new Transmission($config);
         $this->transmission->add('magnet:?xt=urn:btih:11A2AC68A11634E980F265CB1433C599D017A759&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337');
+        return new Response($this->transmission->getStats());
     }
 
     /**
      * @Route("/status", name="test3")
      */
     public function status() {
+        $config = array(
+            'host'     => 'http://127.0.0.1',
+            'endpoint' => '/transmission/rpc',
+            'username' => 'transmission',
+            'password' => '12345678'
+        );
+        $this->transmission = new Transmission($config);
         return new Response($this->transmission->getStats());
     }
 }
