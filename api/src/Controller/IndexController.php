498e7971eb48c9d73c0a51b8338fde92b1eceb09
lbarthon <lsbarthonet2@gmail.com>
2019-03-24 10:45:35 +0100

    Api tests torrents

--- api/src/Controller/IndexController.php
+++ api/src/Controller/IndexController.php
@@ -13,6 +13,9 @@ use Symfony\Component\HttpFoundation\Response;
 use FFMpeg\FFMpeg;
 use FFMpeg\Format\Video\X264;
 
+// Transmission tests
+use Transmission\Transmission;
+
 class IndexController extends AbstractController
 {
     /**
@@ -44,5 +47,30 @@ class IndexController extends AbstractController
             }
         };
         return new Response($encode("Video.avi"));
+    }
+
+    /**
+     * @Route("/torrent", name="test2")
+     */
+    public function dlTorrent() {
+        $torrentDir = $this->getParameter('kernel.project_dir') . '/public/torrents';
+        $transmission = new Transmission();
+        $session = $transmission->getSession();
+        
+        $session->setDownloadDir($torrentDir . '/complete');
+        $session->setIncompleteDir($torrentDir . '/incomplete');
+        $session->setIncompleteDirEnabled(true);
+        $session->save();
+
+        $torrent = $transmission->add($this->getParameter('kernel.project_dir') . '/torrent.torrent');
+        $transmission->start($torrent);
+    }
+
+    /**
+     * @Route("/status", name="test3")
+     */
+    public function status() {
+        $transmission = new Transmission();
+        return new Response($transmission->all());
     }
 }
