42e456ce5bcb1f29e4fab778ba93d3eace2874c9
lbarthon <lsbarthonet2@gmail.com>
2019-03-24 09:13:38 +0100

    Fix

--- api/src/Controller/IndexController.php
+++ api/src/Controller/IndexController.php
@@ -32,26 +32,25 @@ class IndexController extends AbstractController
      * @Route("/test", name="test")
      */
     public function test(Request $request) {
-        $coroutine = new Coroutine($this->encode);
+        $encode = function() {
+            try {
+                $ffmpeg =  FFMpeg::create([
+                    'timeout' => 0
+                ]);
+                $videosDirectory = $this->getParameter('kernel.project_dir');
+                $video = $ffmpeg->open($videosDirectory . '/Video.avi');
+                $mp4Format = new X264();
+                $mp4Format->setAudioCodec("libmp3lame");
+                $video->save($mp4Format, $videosDirectory . '/Video.mp4');
+                yield Awaitable\resolve("Encoding complete.");
+            } catch (\Exception $e) {
+                yield Awaitable\reject($e);
+            }
+        };
+        $coroutine = new Coroutine($encode);
         $coroutine->done(function ($data) {
             echo $data, "\n";
         });
         Loop\run();
-    }
-
-    public function encode() {
-        try {
-            $ffmpeg =  FFMpeg::create([
-                'timeout' => 0
-            ]);
-            $videosDirectory = $this->getParameter('kernel.project_dir');
-            $video = $ffmpeg->open($videosDirectory . '/Video.avi');
-            $mp4Format = new X264();
-            $mp4Format->setAudioCodec("libmp3lame");
-            $video->save($mp4Format, $videosDirectory . '/Video.mp4');
-            yield Awaitable\resolve("Encoding complete.");
-        } catch (\Exception $e) {
-            yield Awaitable\reject($e);
-        }
     }
 }
