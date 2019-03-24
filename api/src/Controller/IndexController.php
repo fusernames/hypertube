06fb26fbdf5f9f6e241412d5934c619af19d97eb
lbarthon <lsbarthonet2@gmail.com>
2019-03-24 12:00:22 +0100

    Try numéro 439082493284 pour faire marcher les torrents

--- api/src/Controller/IndexController.php
+++ api/src/Controller/IndexController.php
@@ -14,17 +14,11 @@ use FFMpeg\FFMpeg;
 use FFMpeg\Format\Video\X264;
 
 // Transmission tests
-use Vohof\Transmission;
+use Transmission\Client;
+use Transmission\Transmission;
 
 class IndexController extends AbstractController
 {
-    public $config = [
-        'host'     => 'http://127.0.0.1',
-        'endpoint' => '/transmission/rpc',
-        'username' => 'foo', // Optional
-        'password' => 'bar' // Optional
-    ];
-
     public $transmission;
 
     /**
@@ -62,10 +56,10 @@ class IndexController extends AbstractController
      * @Route("/torrent", name="test2")
      */
     public function dlTorrent() {
-        $torrentDir = $this->getParameter('kernel.project_dir') . '/public/torrents';
-        $this->transmission  = new Transmission($this->config);
-        $torrent = $this->transmission->add(base64encode(file_get_contents($this->getParameter('kernel.project_dir') . '/torrent.torrent')), true);
-        $this->transmission->start($torrent);
+        $client = new Client();
+        $client->authenticate('username', 'password');
+        $this->transmission = new Transmission();
+        $this->transmission->setClient($client);
     }
 
     /**
