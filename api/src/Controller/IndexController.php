126f9f414b2eb330faf92e139215bdad26553827
lbarthon <lsbarthonet2@gmail.com>
2019-03-24 08:31:15 +0100

    Symfony tests (route /test) -> pushing on aws

--- api/src/Controller/IndexController.php
+++ api/src/Controller/IndexController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+// Use the FFMpeg tool
+use FFMpeg\FFMpeg;
+use FFMpeg\Format\Video\X264;
+
 class IndexController extends AbstractController
 {
     /**
@@ -13,5 +17,20 @@ class IndexController extends AbstractController
     public function index()
     {
         return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test(Request $request) {
+
+        $ffmpeg =  FFMpeg::create();
+        $videosDirectory = $this->get('kernel')->getRootDir() . '/';
+        $video = $ffmpeg->open($videosDirectory . '/Video.avi');
+        $mp4Format = new X264();
+        $mp4Format->setAudioCodec("libmp3lame");
+        $video->save($mp4Format, $videosDirectory . '/Video.mp4');
+
+        return new Response("webm video succesfully converted to mp4");
     }
 }
