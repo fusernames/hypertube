f9c8e18893d8f9e64e593ca5840e37efc971a8cc
lbarthon <lsbarthonet2@gmail.com>
2019-03-28 14:26:57 +0100

    Groups added to lang

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -255,7 +255,7 @@ class User extends BaseUser
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
-     * @Groups({"me"})
+     * @Groups({"me", "get-lang", "set-lang"})
      * @Assert\Length(
      *      max = 255,
      *      maxMessage = "lang cannot be longer than {{ limit }} characters"
