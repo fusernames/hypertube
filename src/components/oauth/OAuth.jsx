b8337c8d4813d37b029d07f0d5dedfc7d7ea9114
lbarthon <lsbarthonet2@gmail.com>
2019-04-01 12:31:18 +0200

    Oauth advancement

--- src/components/oauth/OAuth.jsx
+++ src/components/oauth/OAuth.jsx
@@ -1,18 +1,51 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router'
+import Loading from '../../utils/jsx/Loading'
+import req from '../../utils/req'
+import host from '../../config'
 
 class OAuth extends React.Component {
 
   componentDidMount() {
     const { name } = this.props.match.params;
-
+    let token = "";
+    switch (name) {
+      case "github":
+      case "42":
+        token = this.props.location.query.code;
+        break;
+      case "facebook":
+        // Handle
+        break;
+      case "twitter":
+        // Handle
+        break;
+      case "gmail":
+        // Handle
+        break;
+    }
+    if (token != undefined) {
+      req(host + "/api/oauth", {
+      method: "POST",
+      body: {
+        api: name,
+        token: token
+      }
+      }).then(res => {
+        // Handle api response
+      }).catch(err => {
+        // Handle errors
+      })
+    } else {
+      window.location = host;
+    }
   }
 
   render() {
     const { auth } = this.props
     return (
-      auth.logged ? <Redirect to={{pathname: '/', state: {from: this.props.location}}} /> :  null
+      auth.logged ? <Redirect to={{pathname: '/', state: {from: this.props.location}}} /> :  <Loading />
     )
   }
 }
