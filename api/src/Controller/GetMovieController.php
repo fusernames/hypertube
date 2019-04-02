a62a35b55f1ffdba988b1537f0fa7bbf06898d1e
lbarthon <lsbarthonet2@gmail.com>
2019-04-02 12:35:14 +0200

    Fixes

--- api/src/Controller/GetMovieController.php
+++ api/src/Controller/GetMovieController.php
@@ -10,13 +10,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class GetMovieController extends AbstractController
 {
+    private $_downloadPath = "/var/lib/transmission-daemon/complete/";
+
     public function __invoke(Request $request) {
         // Parsing request's json
         $data = $request->getContent();
         $data = json_decode($data, true);
         if (!isset($data['torrent_link'])) {
-            return new JsonResponse(['error' => 'WRONG_DATA'], 404);
+            return new JsonResponse(['error' => 'WRONG_DATA'], 403);
         }
-        return new JsonResponse(['success' => 'gg negro'], 201);
+        $entityManager = $this->getDoctrine()->getManager();
+        $repository = $entityManager->getRepository(Movie::class);
+        $movie = $repository->findOneBy(['torrentLink' => $data['torrent_link']]);
+        if (file_exists($_downloadPath . $movie->getName())) {
+
+        }
     }
 }
