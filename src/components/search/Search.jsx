78856046434155e401947b1fc99467621706d03f
Alexis CAROFF <alcaroff@e3r9p16.42.fr>
2019-04-03 19:50:14 +0200

    css

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -101,7 +101,7 @@ const styles = {
     position: 'absolute',
     bottom: '0px',
     display:'none',
-    backgroundImage: 'linear-gradient(rgba(0,0,0,0.0), rgba(0,0,0,0.8))',
+    backgroundImage: 'linear-gradient(rgba(0,0,0,0.0), rgba(0,0,0,0.7))',
     width:'100%',
     padding:'10px',
     wordBreak: 'break-word'
