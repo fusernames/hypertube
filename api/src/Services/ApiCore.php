0b6d1173f80e2c55ed78ff78d9ecde904d9bea5a
lbarthon <lsbarthonet2@gmail.com>
2019-04-08 11:44:54 +0200

    Worse dev -> forgotten this

--- api/src/Services/ApiCore.php
+++ api/src/Services/ApiCore.php
@@ -100,7 +100,7 @@ class ApiCore
     {
         $withEmail = $this->userManager->findUserByEmail($userData["email"]);
 
-        $repository = $objectManager->getRepository(OmniAuthInfos::class);
+        $repository = $this->objectManager->getRepository(OmniAuthInfos::class);
         $withOauthId = $repository->findOneBy(["oauthId" => $userData["id"], "name" => $this->getName()]);
 
         if ($withEmail === null && $withOauthId === null) {
