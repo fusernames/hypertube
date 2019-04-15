892d04af3971c587670593c92b2dd6404d7810b8
lbarthon <lsbarthonet2@gmail.com>
2019-04-15 12:34:32 +0200

    React-player removed

--- api/src/Controller/Torrent/GetMovieController.php
+++ api/src/Controller/Torrent/GetMovieController.php
@@ -34,9 +34,26 @@ class GetMovieController extends TorrentController
 
         if (!file_exists($totalPath)) {
             return new JsonResponse(['error' => 'MOVIE_FILE_NULL'], 404);
-    	}
+        }
+
+        // Create the StreamedResponse object
+        $response = new StreamedResponse();
     
-        if (!$movie->getFinished()) {
+        $file = new SplFileObject($totalPath);
+    
+        // Check file existence
+        if (!($file->isFile())) {
+            throw $this->createNotFoundException('Error getting movie at path ' . $totalPath);
+        }
+    
+        // Retrieve file informations
+        $fileName = $file->getBasename();
+        $fileExt  = $file->getExtension();
+        $filePath = $file->getRealPath();
+        
+        if ($movie->getFinished()) {
+            $fileSize = $file->getSize();
+        } else {
             $transmission = new Transmission($this->transmissionConfig);
             $infos = $transmission->get($movie->getTorrentId())['torrents'];
             if (sizeof($infos) !== 1) {
@@ -46,9 +63,98 @@ class GetMovieController extends TorrentController
                 $movie->setFinished(true);
                 $entityManager->persist($movie);
                 $entityManager->flush();
+                $fileSize = $file->getSize();
+            } else {
+                $infos = $infos[0];
+                $movieFile = $this->getMovieFile($infos);
+                $fileSize = $movieFile['bytesCompleted'];
             }
         }
+    
+        $response->headers->set('Accept-Ranges', 'bytes');
+        $response->headers->set('Content-Type', 'video/' . ($fileExt === "mkv" ? "x-matroska" : $fileExt));
+    
+        // Initialise range variables, default to the whole file size
+        $rangeMin = 0;
+        $rangeMax = $fileSize - 1;
+        $rangeStart = $rangeMin;
+        $rangeEnd = $rangeMax;
+    
+        $httpRange = $request->server->get('HTTP_RANGE');
+    
+        // If there's a range, deal with it
+        if ($httpRange) {
+            $isRangeSatisfiable = true;
+    
+            if (preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $httpRange, $matches)) {
+                $rangeStart = intval($matches[1]);
+    
+                if (!empty($matches[2])) {
+                    $rangeEnd = intval($matches[2]);
+                }
+            } else {
+                // Requested HTTP-Range seems invalid.
+                $isRangeSatisfiable = false;
+            }
+    
+            if ($rangeStart <= $rangeEnd) {
+                // If ranges are OK, setting total length
+                $length = $rangeEnd - $rangeStart + 1;
+            } else {
+                // Range start greater than range end -> error somewhere
+                $isRangeSatisfiable = false;
+            }
+    
+            if ($file->fseek($rangeStart) !== 0) {
+                // Error doing fseek, to start reading at the asked byte -> wrong range
+                $isRangeSatisfiable = false;
+            }
+    
+            if ($isRangeSatisfiable) {
+                // Fseek worked -> preparing headers
+                $response->setStatusCode(StreamedResponse::HTTP_PARTIAL_CONTENT);
+    
+                $response->headers->set('Content-Range', sprintf('bytes %d-%d/%d', $rangeStart, $rangeEnd, $fileSize));
+                $response->headers->set('Content-Length', $length);
+                $response->headers->set('Connection', 'Close');
+            } else {
+                // Fseek error -> wrong range -> error
+                $response = new Response();
 
-        return new BinaryFileResponse($totalPath);
+                $response->setStatusCode(StreamedResponse::HTTP_REQUESTED_RANGE_NOT_SATISFIABLE);
+                $response->headers->set('Content-Range', sprintf('bytes */%d', $fileSize));
+    
+                return $response;
+            }
+        } else {
+            // No range, sending whole length
+            $response->headers->set('Content-Length', $fileSize);
+        }
+    
+        // Send headers then
+        $response->prepare($request);
+        $response->sendHeaders();
+    
+        // Read the file and send it
+        $response->setCallback(function () use ($file, $rangeEnd, $fileExt, $totalPath) {
+            $buffer = 1024 * 8;
+
+            $offset = 0;
+            while (!($file->eof()) && (($offset = $file->ftell()) < $rangeEnd)) {
+                set_time_limit(0);
+    
+                if ($offset + $buffer > $rangeEnd) {
+                    $buffer = $rangeEnd + 1 - $offset;
+                }
+
+                echo $file->fread($buffer);
+
+                $file = null;
+            }
+        });
+    
+        // Then everything should be ready, we can send the Response content.
+        // Don't return response because headers, etc. have already been sent.
+        $response->sendContent();
     }
 }
--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import host from '../../config'
-import ReactPlayer from 'react-player';
 
 /**
  * Component takes 2 props =>
@@ -24,11 +23,27 @@ class Player extends Component {
   state = {
     oldTime: 0,
     currentTime: 0,
+    subtitles: {
+      fre: null,
+      eng: null
+    }
+  }
+
+  enableEvent = false
+  player = null
+
+  componentDidMount = () => {
+    let { startTime } = this.props;
+    this.player = document.getElementById("player");
+    if (this.player.currentTime < startTime)
+      this.player.currentTime = startTime;
+    document.addEventListener('keydown', this.handleKeyPress)
+    this.fetchSubtitles()
   }
 
   handleTimeChange = e => {
     this.setStateCheck({
-      currentTime: e.playedSeconds,
+      currentTime: e.target.currentTime,
     }, () => {
       let { currentTime, oldTime } = this.state;
       if (Math.abs(currentTime - oldTime) > 5 && this.props.onChange) {
@@ -36,6 +51,67 @@ class Player extends Component {
         this.props.onChange(currentTime);
       }
     })
+  }
+
+  fetchSubtitles = (redo = true) => {
+    const { movieId } = this.props
+    const { fre, eng } = this.state.subtitles
+    fetch(host + '/api/movies/subtitles/' + movieId + '/eng')
+    .then(res => {
+      this.setStateCheck({
+        ...this.state,
+        subtitles: {
+          ...this.state.subtitles,
+          eng: res.status === 200
+        }
+      })
+    })
+    .catch(err => {
+      // Handle err
+    })
+    fetch(host + '/api/movies/subtitles/' + movieId + '/fre')
+    .then(res => {
+      this.setStateCheck({
+        ...this.state,
+        subtitles: {
+          ...this.state.subtitles,
+          fre: res.status === 200
+        }
+      })
+    })
+    .catch(err => {
+      // Handle err
+    })
+    if (!fre && !eng && redo) {
+      setTimeout(() => this.fetchSubtitles(false), 2500);
+    }
+  }
+
+  handleKeyPress = e => {
+    const { player } = this
+    if (!this.enableEvent || !this.player) return
+    if (document.activeElement.tagName === "INPUT") return
+    let needsPrevent = true
+    switch (e.key) {
+      case " ":
+        player.paused ? player.play() : player.pause()
+        break
+      case "ArrowRight":
+        player.currentTime += 10
+        break
+      case "ArrowLeft":
+        player.currentTime -= 10
+        break
+      case "ArrowUp":
+        player.volume > .9 ? player.volume = 1 : player.volume += .1
+        break
+      case "ArrowDown":
+        player.volume < .1 ? player.volume = 0 : player.volume -= .1
+        break
+      default:
+        needsPrevent = false
+    }
+    if (needsPrevent) e.preventDefault()
   }
 
   componentWillMount() {
@@ -47,28 +123,18 @@ class Player extends Component {
   }
 
   render() {
-    const { mediaUrl, movieId, locales, startTime } = this.props
-    const { currentTime } = this.state
+    const { mediaUrl, movieId, locales } = this.props
+    const { fre, eng } = this.state.subtitles
     if (!mediaUrl) return null
     return (
-      <div id="player">
-        <ReactPlayer
-          ref="reactPlayer"
-          controls={true}
-          width='100%'
-          url={mediaUrl}
-          onProgress={this.handleTimeChange}
-          config={{
-            file: {
-              tracks: [
-                {kind: 'subtitles', src: host + '/api/movies/subtitles/' + movieId + '/eng', srcLang: 'en', label: 'English'},
-                {kind: 'subtitles', src: host + '/api/movies/subtitles/' + movieId + '/fre', srcLang: 'fr', label: 'Français', default: locales.code === "fr"}
-              ]
-            }
-          }}
-        />
-        {this.refs.reactPlayer && startTime > currentTime ? this.refs.reactPlayer.seekTo(startTime, 'seconds') : ''}
-      </div>
+      <video id="player" controls style={{width: '100%'}}
+          onTimeUpdate={this.handleTimeChange}
+          onMouseEnter={() => this.enableEvent = true}
+          onMouseLeave={() => this.enableEvent = false}>
+        <source src={mediaUrl} />
+        {eng && <track label="English" kind="subtitles" srcLang="en" src={host + '/api/movies/subtitles/' + movieId + '/eng'} />}
+        {fre && <track label="Français" kind="subtitles" srcLang="fr" src={host + '/api/movies/subtitles/' + movieId + '/fre'} default={locales.code === "fr"}/>}
+      </video>
     );
   }
 
