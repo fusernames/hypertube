62f792f03f77eea82f0ec40e21ac8a24ca09cbd5
lbarthon <lsbarthonet2@gmail.com>
2019-04-15 13:46:58 +0200

    little node server

---
+++ server.js
@@ -0,0 +1,26 @@
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+const express = require('express');
+
+var app = express();
+
+app.get("mkv", (req, res) => {
+    var videoPath = req.query.path;
+    try {
+        if (fs.existsSync(videoPath)) {
+            var stream = fs.createReadStream(videoPath);
+            ffmpeg
+                .input(stream)
+                .outputFormat('mp4')
+                .on('error', console.error)
+                .inputFormat('mkv')
+                .audioCodec('aac')
+                .videoCodec('libx264')
+                .pipe(res)
+        }
+    } catch (err) {
+        console.log("Error, unknown file");
+    }
+})
+
+app.listen(8080);
