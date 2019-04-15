770656810401dca2ff69bfd0056d7f954734a12f
lbarthon <lsbarthonet2@gmail.com>
2019-04-15 12:22:10 +0200

    React player all good

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -24,27 +24,11 @@ class Player extends Component {
   state = {
     oldTime: 0,
     currentTime: 0,
-    subtitles: {
-      fre: null,
-      eng: null
-    }
-  }
-
-  enableEvent = false
-  player = null
-
-  componentDidMount = () => {
-    let { startTime } = this.props;
-    this.player = document.getElementById("player");
-    if (this.player.currentTime < startTime)
-      this.player.currentTime = startTime;
-    document.addEventListener('keydown', this.handleKeyPress)
-    this.fetchSubtitles()
   }
 
   handleTimeChange = e => {
     this.setStateCheck({
-      currentTime: e.target.currentTime,
+      currentTime: e.playedSeconds,
     }, () => {
       let { currentTime, oldTime } = this.state;
       if (Math.abs(currentTime - oldTime) > 5 && this.props.onChange) {
@@ -52,67 +36,6 @@ class Player extends Component {
         this.props.onChange(currentTime);
       }
     })
-  }
-
-  fetchSubtitles = (redo = true) => {
-    const { movieId } = this.props
-    const { fre, eng } = this.state.subtitles
-    fetch(host + '/api/movies/subtitles/' + movieId + '/eng')
-    .then(res => {
-      this.setStateCheck({
-        ...this.state,
-        subtitles: {
-          ...this.state.subtitles,
-          eng: res.status === 200
-        }
-      })
-    })
-    .catch(err => {
-      // Handle err
-    })
-    fetch(host + '/api/movies/subtitles/' + movieId + '/fre')
-    .then(res => {
-      this.setStateCheck({
-        ...this.state,
-        subtitles: {
-          ...this.state.subtitles,
-          fre: res.status === 200
-        }
-      })
-    })
-    .catch(err => {
-      // Handle err
-    })
-    if (!fre && !eng && redo) {
-      setTimeout(() => this.fetchSubtitles(false), 2500);
-    }
-  }
-
-  handleKeyPress = e => {
-    const { player } = this
-    if (!this.enableEvent || !this.player) return
-    if (document.activeElement.tagName === "INPUT") return
-    let needsPrevent = true
-    switch (e.key) {
-      case " ":
-        player.paused ? player.play() : player.pause()
-        break
-      case "ArrowRight":
-        player.currentTime += 10
-        break
-      case "ArrowLeft":
-        player.currentTime -= 10
-        break
-      case "ArrowUp":
-        player.volume > .9 ? player.volume = 1 : player.volume += .1
-        break
-      case "ArrowDown":
-        player.volume < .1 ? player.volume = 0 : player.volume -= .1
-        break
-      default:
-        needsPrevent = false
-    }
-    if (needsPrevent) e.preventDefault()
   }
 
   componentWillMount() {
@@ -124,24 +47,28 @@ class Player extends Component {
   }
 
   render() {
-    const { mediaUrl, movieId, locales } = this.props
-    const { fre, eng } = this.state.subtitles
+    const { mediaUrl, movieId, locales, startTime } = this.props
+    const { currentTime } = this.state
     if (!mediaUrl) return null
     return (
-      <ReactPlayer
-        id="player"
-        controls={true}
-        style={{width: '100%'}}
-        url={mediaUrl}
-        config={{
-          file: {
-            tracks: [
-              {kind: 'subtitles', src: host + '/api/movies/subtitles/' + movieId + '/eng', srcLang: 'en', label: 'English'},
-              {kind: 'subtitles', src: host + '/api/movies/subtitles/' + movieId + '/fre', srcLang: 'fr', label: 'Français', default: locales.code === "fr"}
-            ]
-          }
-        }}
-      />
+      <div id="player">
+        <ReactPlayer
+          ref="reactPlayer"
+          controls={true}
+          width='100%'
+          url={mediaUrl}
+          onProgress={this.handleTimeChange}
+          config={{
+            file: {
+              tracks: [
+                {kind: 'subtitles', src: host + '/api/movies/subtitles/' + movieId + '/eng', srcLang: 'en', label: 'English'},
+                {kind: 'subtitles', src: host + '/api/movies/subtitles/' + movieId + '/fre', srcLang: 'fr', label: 'Français', default: locales.code === "fr"}
+              ]
+            }
+          }}
+        />
+        {this.refs.reactPlayer && startTime > currentTime ? this.refs.reactPlayer.seekTo(startTime, 'seconds') : ''}
+      </div>
     );
   }
 
